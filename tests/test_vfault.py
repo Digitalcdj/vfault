28a3f5770858439810ea6f871856b68c9df4f7e2
Ian Fraser <digitalcdj@hotmail.co.uk>
2026-08-22 23:50:02 +0100

    Add files via upload

--- tests/test_vfault.py
+++ tests/test_vfault.py
@@ -11,7 +11,7 @@ import sys
 import pytest
 
 # Ensure vfault module is importable
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import vfault
 
